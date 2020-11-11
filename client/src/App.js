5e5e632bb55e0d884ef7e9283b25c7a1a8438dac
Connor <connorskorburg@gmail.com>
2020-11-10 18:55:00 -0600

    added material UI for styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div style={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
