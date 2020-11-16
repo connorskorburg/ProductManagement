a89e93e1d287c02d88b628b064f63c53bda5b49e
Connor <connorskorburg@gmail.com>
2020-11-15 20:43:51 -0600

    added bootstrap and custom css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Navigation from '../reusables/Navigation';
 
 const Home = () => {
-  return (
-    <>
-      <Navigation />
-    </>
-  )
-}
+	return (
+		<Fragment>
+			<Navigation page="home" />
+			<Container
+				style={{ height: '100vh' }}
+				className="d-flex justify-content-center align-items-center hero text-white"
+				fluid
+			>
+				<div className="text-center px-3">
+					<h1 className="w-100 font-weight-bold">Product Manager</h1>
+					<p className="lead py-3">Manage your professional and personal inventory all in one place</p>
+					<Nav className="btn-container">
+						<NavDropdown
+							className="btn bg-light"
+							title={<span className="text-brand font-weight-bold">Login</span>}
+							id="login-dropdown-hero"
+						>
+							<Form className="p-3 form-home">
+								<FormControl type="text" placeholder="Email" className="mr-sm-2 mt-2" />
+								<FormControl type="password" placeholder="Password" className="mr-sm-2 mt-2" />
+								<Button variant="success" className="btn-block mt-2">
+									Login
+								</Button>
+							</Form>
+						</NavDropdown>
+						<NavDropdown
+							className="btn bg-brand-alt"
+							title={<span className="text-white font-weight-bold">Get Started</span>}
+							id="register-dropdown-hero"
+						>
+							<Form className="p-3 form-home">
+								<FormControl type="text" placeholder="First Name" className="mr-sm-2 mt-2" />
+								<FormControl type="text" placeholder="Last Name" className="mr-sm-2 mt-2" />
+								<FormControl type="text" placeholder="Email" className="mr-sm-2 mt-2" />
+								<FormControl type="password" placeholder="Password" className="mr-sm-2 mt-2" />
+								<FormControl type="password" placeholder="Confirm Password" className="mr-sm-2 mt-2" />
+								<Button variant="success" className="btn-block mt-2">
+									Register
+								</Button>
+							</Form>
+						</NavDropdown>
+					</Nav>
+				</div>
+			</Container>
+		</Fragment>
+	);
+};
 
-export default Home
+export default Home;
--- client/src/reusables/Navigation.js
+++ client/src/reusables/Navigation.js
@@ -6,25 +6,27 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-const Navigation = () => {
+const Navigation = (props) => {
+  console.log({ props });
   return (
-    <Navbar fixed='top' expand="lg" className='px-5'>
-      <Navbar.Brand>PRODMNG</Navbar.Brand>
+    <Navbar expand="lg" bg='light' fixed='top' className='px-5-l'>
+      <Navbar.Brand className='font-weight-bold text-brand'>PRODMNG</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <NavDropdown title="Login" id="basic-nav-dropdown">
-            <Form className='p-3 form-home'>
+      <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav className='text-brand'>
+          <NavDropdown title={
+            <span className='text-brand'>Login</span>
+          } id="login-dropdown">
+            <Form className='p-3 form-home bg-light'>
               <FormControl type="text" placeholder="Email" className="mr-sm-2 mt-2" />
               <FormControl type="password" placeholder="Password" className="mr-sm-2 mt-2" />
               <Button variant="success" className='btn-block mt-2'>Login</Button>
             </Form>
           </NavDropdown>
-          <NavDropdown title="Register" id="basic-nav-dropdown">
-            <Form className='p-3 form-home'>
+          <NavDropdown title={
+            <span className='text-brand'>Register</span>
+          } id="register-dropdown">
+            <Form className='p-3 form-home bg-light'>
               <FormControl type="text" placeholder="First Name" className="mr-sm-2 mt-2" />
               <FormControl type="text" placeholder="Last Name" className="mr-sm-2 mt-2" />
               <FormControl type="text" placeholder="Email" className="mr-sm-2 mt-2" />
@@ -33,6 +35,13 @@ const Navigation = () => {
               <Button variant="success" className='btn-block mt-2'>Register</Button>
             </Form>
           </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link className='text-brand font-weight-bold ml-lg-4' href="#home">Home</Nav.Link>
+          <Nav.Link className='text-brand font-weight-bold ml-lg-4' href="#about">About</Nav.Link>
+          <Nav.Link className='text-brand font-weight-bold ml-lg-4' href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
