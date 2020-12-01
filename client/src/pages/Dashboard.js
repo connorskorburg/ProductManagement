c4c0cb2d65e5562d2693365c6a645b6652b22140
Connor <connorskorburg@gmail.com>
2020-11-30 22:02:33 -0600

    added dashboard page with navbar setup

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Navigation from '../reusables/Navigation';
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navigation page='dashboard' />
+    </>
+  )
+}
+
+export default Dashboard;
